619b6bb5e6206830bae32b88a50a2f5dda88dbcc
Renan Monteiro <renan.paula@pmrun.com.br>
2024-06-16 18:46:47 -0300

    feat: created repositories

---
+++ backend/src/main/java/com/eventostec/api/repositories/AddressRepository.java
@@ -0,0 +1,11 @@
+package com.eventostec.api.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eventostec.api.domain.address.Address;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+    
+}
---
+++ backend/src/main/java/com/eventostec/api/repositories/EventRepository.java
@@ -0,0 +1,11 @@
+package com.eventostec.api.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eventostec.api.domain.event.Event;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+    
+}
