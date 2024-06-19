6976fadc26cda980cf66203be62e9dfde01d392b
Renan Monteiro <renan.paula@pmrun.com.br>
2024-06-19 11:56:01 -0300

    feat: added getEvents method

--- backend/src/main/java/com/eventostec/api/controller/EventController.java
+++ backend/src/main/java/com/eventostec/api/controller/EventController.java
@@ -1,7 +1,10 @@
 package com.eventostec.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.service.EventService;
 
 @RestController
@@ -34,6 +38,13 @@ public class EventController {
         Event newEvent = this.eventService.createEvent(eventRequestDTO);
 
         return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+
+        List<EventResponseDTO> allEvents = this.eventService.getEvents(page, size);
+        return ResponseEntity.ok(allEvents);
     }
     
 }
--- backend/src/main/java/com/eventostec/api/service/EventService.java
+++ backend/src/main/java/com/eventostec/api/service/EventService.java
@@ -4,17 +4,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.repositories.EventRepository;
 
 @Service
@@ -78,5 +83,16 @@ public class EventService {
         fos.close();
 
         return convFile;
+    }
+
+    public List<EventResponseDTO> getEvents(int page, int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Event> eventPage = this.repository.findAll(pageable);
+
+        return eventPage.map(event -> new EventResponseDTO(
+            event.getId(), event.getTitle(), event.getDescription(), event.getDate(), "", "", event.getRemote(), event.getEventUrl(), event.getImgUrl()
+        )).stream().toList();
     }
 }
