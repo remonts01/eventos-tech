144bc312d3868826a18c7bc385d4a49efcab22e6
Renan Monteiro <renan.paula@pmrun.com.br>
2024-06-19 18:09:50 -0300

    refactor: added field address

--- backend/src/main/java/com/eventostec/api/domain/event/Event.java
+++ backend/src/main/java/com/eventostec/api/domain/event/Event.java
@@ -3,9 +3,13 @@ package com.eventostec.api.domain.event;
 import java.util.Date;
 import java.util.UUID;
 
+import com.eventostec.api.domain.address.Address;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -35,4 +39,7 @@ public class Event {
     private Boolean remote;
 
     private Date date;
+
+    @OneToOne(mappedBy = "event", cascade = CascadeType.ALL)
+    private Address address;
 }
